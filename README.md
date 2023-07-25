a01f96313ca2990e36df6f864b502c6d3063ee02
KETAK12 <ketak.hello@gmail.com>
2023-07-26 03:03:08 +0530

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Recruitment-Management-Portal
+Front-end project for a Recruitment Management Portal that helps co-ordinate events, and recruit volunteers.
+
+This is a finished front-end design for a Recruitment Management Portal which targets entertainment events, to help them recruit part-time employees 
+or Volunteers which are interested in Event Management.
---
+++ assets/js/btnJS.js
@@ -0,0 +1,166 @@
+document.getElementById("LearnMore").style.borderColor = "white";
+document.getElementById("LearnMore").style.background = "transparent";
+document.getElementById("LearnMore").style.color = "white";
+
+document.getElementById("LearnMore").addEventListener("mouseover", function() {
+
+    document.getElementById("LearnMore").style.borderColor = "white";
+    document.getElementById("LearnMore").style.background = "white";
+    document.getElementById("LearnMore").style.color = "#9b59b6";
+
+});
+
+document.getElementById("LearnMore").addEventListener("mouseout", function() {
+
+    document.getElementById("LearnMore").style.borderColor = "white";
+    document.getElementById("LearnMore").style.background = "transparent";
+    document.getElementById("LearnMore").style.color = "white";
+
+});
+
+
+document.getElementById("LearnMore-1").style.borderColor = "white";
+document.getElementById("LearnMore-1").style.background = "transparent";
+document.getElementById("LearnMore-1").style.color = "white";
+
+document.getElementById("LearnMore-1").addEventListener("mouseover", function() {
+
+    document.getElementById("LearnMore-1").style.borderColor = "white";
+    document.getElementById("LearnMore-1").style.background = "white";
+    document.getElementById("LearnMore-1").style.color = "#9b59b6";
+
+});
+
+document.getElementById("LearnMore-1").addEventListener("mouseout", function() {
+
+    document.getElementById("LearnMore-1").style.borderColor = "white";
+    document.getElementById("LearnMore-1").style.background = "transparent";
+    document.getElementById("LearnMore-1").style.color = "white";
+
+});
+
+
+document.getElementById("LearnMore-2").style.borderColor = "white";
+document.getElementById("LearnMore-2").style.background = "transparent";
+document.getElementById("LearnMore-2").style.color = "white";
+
+document.getElementById("LearnMore-2").addEventListener("mouseover", function() {
+
+    document.getElementById("LearnMore-2").style.borderColor = "white";
+    document.getElementById("LearnMore-2").style.background = "white";
+    document.getElementById("LearnMore-2").style.color = "#9b59b6";
+
+});
+
+document.getElementById("LearnMore-2").addEventListener("mouseout", function() {
+
+    document.getElementById("LearnMore-2").style.borderColor = "white";
+    document.getElementById("LearnMore-2").style.background = "transparent";
+    document.getElementById("LearnMore-2").style.color = "white";
+
+});
+
+
+
+document.getElementById("LearnMore-3").style.borderColor = "white";
+document.getElementById("LearnMore-3").style.background = "transparent";
+document.getElementById("LearnMore-3").style.color = "white";
+
+document.getElementById("LearnMore-3").addEventListener("mouseover", function() {
+
+    document.getElementById("LearnMore-3").style.borderColor = "white";
+    document.getElementById("LearnMore-3").style.background = "white";
+    document.getElementById("LearnMore-3").style.color = "#9b59b6";
+
+});
+
+document.getElementById("LearnMore-3").addEventListener("mouseout", function() {
+
+    document.getElementById("LearnMore-3").style.borderColor = "white";
+    document.getElementById("LearnMore-3").style.background = "transparent";
+    document.getElementById("LearnMore-3").style.color = "white";
+
+});
+
+
+document.getElementById("LearnMore-4").style.borderColor = "white";
+document.getElementById("LearnMore-4").style.background = "transparent";
+document.getElementById("LearnMore-4").style.color = "white";
+
+document.getElementById("LearnMore-4").addEventListener("mouseover", function() {
+
+    document.getElementById("LearnMore-4").style.borderColor = "white";
+    document.getElementById("LearnMore-4").style.background = "white";
+    document.getElementById("LearnMore-4").style.color = "#9b59b6";
+
+});
+
+document.getElementById("LearnMore-4").addEventListener("mouseout", function() {
+
+    document.getElementById("LearnMore-4").style.borderColor = "white";
+    document.getElementById("LearnMore-4").style.background = "transparent";
+    document.getElementById("LearnMore-4").style.color = "white";
+
+});
+
+document.getElementById("LearnMore-5").style.borderColor = "white";
+document.getElementById("LearnMore-5").style.background = "transparent";
+document.getElementById("LearnMore-5").style.color = "white";
+
+document.getElementById("LearnMore-5").addEventListener("mouseover", function() {
+
+    document.getElementById("LearnMore-5").style.borderColor = "white";
+    document.getElementById("LearnMore-5").style.background = "white";
+    document.getElementById("LearnMore-5").style.color = "#9b59b6";
+
+});
+
+document.getElementById("LearnMore-5").addEventListener("mouseout", function() {
+
+    document.getElementById("LearnMore-5").style.borderColor = "white";
+    document.getElementById("LearnMore-5").style.background = "transparent";
+    document.getElementById("LearnMore-5").style.color = "white";
+
+});
+
+
+
+document.getElementById("LearnMore-6").style.borderColor = "white";
+document.getElementById("LearnMore-6").style.background = "transparent";
+document.getElementById("LearnMore-6").style.color = "white";
+
+document.getElementById("LearnMore-6").addEventListener("mouseover", function() {
+
+    document.getElementById("LearnMore-6").style.borderColor = "white";
+    document.getElementById("LearnMore-6").style.background = "white";
+    document.getElementById("LearnMore-6").style.color = "#9b59b6";
+
+});
+
+document.getElementById("LearnMore-6").addEventListener("mouseout", function() {
+
+    document.getElementById("LearnMore-6").style.borderColor = "white";
+    document.getElementById("LearnMore-6").style.background = "transparent";
+    document.getElementById("LearnMore-6").style.color = "white";
+
+});
+
+document.getElementById("LearnMore-7").style.borderColor = "white";
+document.getElementById("LearnMore-7").style.background = "transparent";
+document.getElementById("LearnMore-7").style.color = "white";
+
+document.getElementById("LearnMore-7").addEventListener("mouseover", function() {
+
+    document.getElementById("LearnMore-7").style.borderColor = "white";
+    document.getElementById("LearnMore-7").style.background = "white";
+    document.getElementById("LearnMore-7").style.color = "#9b59b6";
+
+});
+
+document.getElementById("LearnMore-7").addEventListener("mouseout", function() {
+
+    document.getElementById("LearnMore-7").style.borderColor = "white";
+    document.getElementById("LearnMore-7").style.background = "transparent";
+    document.getElementById("LearnMore-7").style.color = "white";
+
+});
